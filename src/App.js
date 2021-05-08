183ca544bbc694e11b985d92c5346b0846f74fec
jeonghun94 <66016555+jeonghun94@users.noreply.github.com>
2021-05-09 00:11:35 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,110 +1,48 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
+class App extends React.Component{
 
-function Member({name, image, rank}) {
-  return (
-    <div>
-      <h3>I love {name} / {rank}</h3>
-      <img alt='load fail' src={image} />
-    </div>
-  );
-}
-
-const blackPink =[
-  {
-    id:1,
-    rank:2,
-    name:'로제',
-    image:'http://www.newscj.com/news/photo/202103/newscj_%EC%B2%9C%EC%A7%80%EC%9D%BC%EB%B3%B4_2021_3_21_184412_888183.jpg'
-  },
-  {
-    id:2,
-    rank:1,
-    name:'지수',
-    image:'https://image.fmkorea.com/files/attach/new/20200509/486616/33498427/2899343737/d5334a04b6bf2dff1bda599999e08959.jpg'
-  },
-  {
-    id:3,
-    rank:3,
-    name:'제니',
-    image:'https://dispatch.cdnser.be/cms-content/uploads/2020/10/11/93235d9c-e8b3-4ac5-9edd-0c63b2079a59.jpeg'
-  },
-  {
-    id:4,
-    rank:4,
-    name:'리사',
-    image:'https://image.news1.kr/system/photos/2020/10/26/4446138/article.jpg/dims/optimize'
+  constructor(props){
+    super(props)
+    console.log('render');
   }
-];
 
-Member.propTypes ={
-  name:PropTypes.string.isRequired,
-  image:PropTypes.string.isRequired,
-  rank:PropTypes.number
+  state = {
+    count : 0
+  };
+
+  add  = () => {
+    this.setState(c => ({count: c.count + 1}));
+  };
+
+  min = () => {
+    this.setState(c => ({count: c.count -1}));
+  };
+
+  componentDidMount(){
+    console.log('mount');
+  }
+
+  componentDidUpdate(){
+    console.log('update');
+  }
+
+  componentWillUnmount(){
+    console.log('unmount');
+  }
+
+  render(){
+    console.clear();
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.min}>Min</button>
+      </div>
+    )
+  }
 }
-
-function App() {
-  
-  return(
-    <div>
-      { 
-        blackPink.map(i =>
-          <Member key={i.id} rank={i.rank} name={i.name} image={i.image}/> 
-        )
-      }
-    </div>
-  );
-
-}
-
-// class App extends React.Component{
-
-//   constructor(props){
-//     super(props);
-//     console.log('hello');
-//   }
-
-//   state = {
-//     isLoading:true,
-//     movie:[],
-//   };
-
-//   // add = () =>{
-//   //   // console.log('add');
-//   //   this.setState({count:this.state.count +1})
-//   // }
-
-//   // minus = () =>{
-//   //   // console.log('minus');
-//   //   this.setState({count:this.state.count -1})
-//   // }
-
-//   componentDidMount(){
-//     console.log('didmount');
-
-//     setTimeout(() => {
-//       this.setState({isLoading:false});
-//     },3000);
-//   }
-  
-//   // componentDidUpdate(){
-//   //   console.log('didupdate');
-//   // }
-
-//   // componentWillUnmount(){
-//   //   console.log('unmount');
-//   // }
-
-//   render(){
-//     console.log('hello2');
-//     const {isLoading} = this.state;
-//     return (
-//       <div>{isLoading ? 'Loading...' : 'We are ready'}
-        
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
